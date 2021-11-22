a8407933bd8dd2dcabe70a9df7d9d76882bffd11
sangha110495 <48673702+sangHa0411@users.noreply.github.com>
2021-11-22 10:18:13 +0900

    Update crawler.py

--- crawler.py
+++ crawler.py
@@ -5,33 +5,22 @@ import parmap
 from bs4 import BeautifulSoup
 from article import ArticleCrawler
 from writer import Writer
-from tqdm import tqdm
 
-def crawl_data(base_url : str, size : int) -> list :
-    article_data = []
-    for i in tqdm(range(1,size)) :
-        try :
-            url = base_url + '?page=' + str(i)
-    
-            response = requests.get(url)
-            bs = BeautifulSoup(response.text, 'html.parser')
-            
-            page_url = bs.find('link' , {'rel' : 'canonical'})
-            page_url = page_url['href']
-            
-            if url != page_url :
-                break
-    
-            article_info = bs.findAll('a', {'data-link-name' : 'article'})
-        
-            article_list = [article['href'] for article in article_info]
-            article_list = list(set(article_list))
-        
-            article_data.extend(article_list)
-        except :
-            continue
-    article_data = list(set(article_data))
-    return article_data
+def crawl_data(index : int, base_url: str) -> list :
+    try :
+        url = base_url + '?page=' + str(index)
+        response = requests.get(url)
+        bs = BeautifulSoup(response.text, 'html.parser')
+
+        page_url = bs.find('link' , {'rel' : 'canonical'})
+        page_url = page_url['href']            
+
+        article_info = bs.findAll('a', {'data-link-name' : 'article'})
+        article_list = [article['href'] for article in article_info]
+        article_list = list(set(article_list))        
+        return article_list
+    except :
+        return None
 
 def save_data(article_url: str, article_crawler: ArticleCrawler, writer: Writer) -> tuple :
     try :
@@ -45,28 +34,34 @@ if __name__ == '__main__' :
     DATA_SIZE = 1900
     BASE_URL = 'https://www.theguardian.com/'
     CATEGORY = ['world', 
+        'world/europe-news',
         'uk-news', 
         'technology' , 
         'business' , 
         'environment' , 
-        'climate-crisis',
+        'environment/climate-crisis',
         'science',
+        'culture',
         'global-development',
         'technology',
-        'coronavirus-outbreak'
+        'sport',
+        'world/coronavirus-outbreak'
     ]
 
-    num_cores = multiprocessing.cpu_count()
+    num_cores = multiprocessing.cpu_count() // 2
+    print('The numvber of Cores : %d \n' %num_cores)
 
     article_data = []
     for cate in CATEGORY :
         print('Category : %s' %cate)
         url = BASE_URL+cate
-        article_list = crawl_data(url, DATA_SIZE)
-        print('Size of articles : %d\n' %len(article_list))
+        article_list = parmap.map(crawl_data, range(1,DATA_SIZE+1), url, pm_pbar=True, pm_processes=num_cores) 
+
+        article_list = [article for article in article_list if article != None]
+        article_list = sum(article_list, [])
         article_data.extend(article_list)
+        print('Size of articles : %d\n' %len(article_list))
         
-    article_data = [article for article in article_data if isinstance(article, str)]
     article_data = list(set(article_data))
     print('Total size of data : %d ' %len(article_data))
     
@@ -75,9 +70,8 @@ if __name__ == '__main__' :
    
     article_crawler = ArticleCrawler()
     writer = Writer('./Data')
-
-    article_info = [writer(article) for article in article_data]
-    article_info = [info for info in article_info if isinstance(info, tuple)]
+    article_info = parmap.map(save_data, article_data, article_crawler, writer, pm_pbar=True, pm_processes=num_cores) 
+    article_info = [info for info in article_info if info != None]
 
     info_df = pd.DataFrame(article_info, columns=['title', 'date', 'category', 'text'])
     info_df.to_csv('./Info/theguardians_article_info.csv')
